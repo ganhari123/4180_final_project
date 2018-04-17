ca107d68ba66029ab2a1cc34f17625505bfbb80a
Izaak <izaak@DESKTOP-RGMR5NJ.localdomain>
2018-04-17 19:17:24 -0400

    Filled out the sections for threads, basic falling detection, and sending the characters over bluetooth.

--- Mbed/4180_FinalProj_IMU.cpp
+++ Mbed/4180_FinalProj_IMU.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 #include "rtos.h"
+#include <math.h>
 
 RawSerial  pc(USBTX, USBRX);
 RawSerial  bluetooth(p13,p14);
@@ -8,7 +9,7 @@ Thread t_imu;
 Thread t_heartrate;
 
 volatile char heartrate;
-volatile char has_fallen; 
+volatile char has_fallen = 0; 
 
 /*****
 TODO:
@@ -18,14 +19,25 @@ in the main loop, fire message to phone using "G" as the header then the hearrat
 main will wait .25 before firing another (letting each other thread run as quickly as they want to)
 
 *****/
+
+LSM9DS1 imu(p9, p10, 0xD6, 0x3C);
  
-void imu() {
+void imu_fall_check() {
     while(1) {
-        Thread::wait(1000);
+	//Calculate the magnitude of the accel vector. If above 4.0 (from research graphs) we will mark it as a fall.
+	imu.readMag();
+	float accel_mag = sqrt(imu.ax*imu.ax + imu.ay*imu.ay + imu.az*imu.az);
+	if(accel_mag > 4.0) {
+	    has_fallen = 1;  
+	    //Wait for half a second to make sure that the fall gets sent properly by the main thread.
+	    Thread::wait(500);
+	} else {
+	    has_fallen = 0;
+	}
     }
 }
  
-void heartrate() {
+void read_heartrate() {
     while(1) {
         Thread::wait(1000);
     }
@@ -53,11 +65,22 @@ int main()
     pc.attach(&pc_recv, Serial::RxIrq);
     bluetooth.attach(&bluetooth_recv, Serial::RxIrq);
 
-    t_imu.start(imu);
-    t_heartrate.start(heartrate);
+    //IMU init stuff
+    imu.begin();
+    if( !imu.begin() ) {
+	pc.printf("Failed to start IMU.");
+    }
+    imu.calibrate();
+
+    //Start threads
+    t_imu.start(imu_fall_check);
+    t_heartrate.start(read_heartrate);
 
     //Main thread will fire a packet every .25 seconds
     while(1) {
-        Thread::wait(1000);
+	bluetooth.putc('G');
+	bluetooth.putc(heartrate);
+	bluetooth.putc(has_fallen);
+        Thread::wait(250);
     }
 }
